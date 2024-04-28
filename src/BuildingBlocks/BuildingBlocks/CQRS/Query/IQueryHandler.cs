75171748aa2b3617f93d105c842ded15ee84fe5c
Piotr Gamorski <pgamorski@wp.pl>
2024-04-28 20:48:12 +0200

    GetProduts, GetProductById and GetProductByCategory endpoints created

--- src/BuildingBlocks/BuildingBlocks/CQRS/Query/IQueryHandler.cs
+++ src/BuildingBlocks/BuildingBlocks/CQRS/Query/IQueryHandler.cs
@@ -2,7 +2,7 @@
 
 namespace BuildingBlocks.CQRS.Query
 {
-    internal interface IQueryHandler<in TQuery, TResponse>
+    public interface IQueryHandler<in TQuery, TResponse>
         : IRequestHandler<TQuery, TResponse>
         where TQuery : IQuery<TResponse>
         where TResponse : notnull
