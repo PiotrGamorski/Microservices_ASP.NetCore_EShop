6f57c7d726817dc4a7976614219c1936d8992bdb
Piotr Gamorski <pgamorski@wp.pl>
2024-05-02 10:17:27 +0200

    DeleteProductCommandHandler changed to handle deletion when product not found

--- src/Services/Catalog/Catalog_API/Products/DeleteProduct/DeleteProductHandler.cs
+++ src/Services/Catalog/Catalog_API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -1,20 +1,22 @@
-﻿
-
-namespace Catalog_API.Products.DeleteProduct
+﻿namespace Catalog_API.Products.DeleteProduct
 {
     public record DeleteProductCommand(Guid Id) : ICommand<DeleteProductResult>;
     public record DeleteProductResult(bool IsSuccess);
 
     public class DeleteProductCommandHandler
-        (IDocumentSession session, ILogger logger)
+        (IDocumentSession session, ILogger<DeleteProductCommandHandler> logger)
         : ICommandHandler<DeleteProductCommand, DeleteProductResult>
     {
         public async Task<DeleteProductResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
         {
             logger.LogInformation("DeleteProductCommandHandler.Handle called with {@Command}", command);
 
+            var product = await session.LoadAsync<Product>(command.Id);
+           
+            if (product == null) return new DeleteProductResult(false);
+
             session.Delete<Product>(command.Id);
-            await session.SaveChangesAsync();
+            await session.SaveChangesAsync(cancellationToken);
 
             return new DeleteProductResult(true);
         }
